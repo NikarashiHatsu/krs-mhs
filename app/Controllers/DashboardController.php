add3ab427e98b2045b6599ca1affb6bdd2cac247
Muhamad Ahmadin <ahmadinations@gmail.com>
2022-02-05 00:05:20 +0700

    feat: dashboard count and tomobol detil

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -3,11 +3,18 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Matakuliah;
+use App\Models\User;
 
 class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('dashboard/index');
+        $user = new User();
+        $matakuliah = new Matakuliah();
+        $data['jumlah_mahasiswa'] = $user->where('role', 'mahasiswa')->countAllResults();
+        $data['jumlah_dosen'] = $user->where('role', 'dosen')->countAllResults();
+        $data['jumlah_matakuliah'] = $matakuliah->countAll();
+        return view('dashboard/index', $data);
     }
 }
