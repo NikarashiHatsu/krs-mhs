51a91748f71365af1ea78737c5a8d355daa472ab
NikarashiHatsu <yourlovelydev@gmail.com>
2022-02-05 01:18:23 +0700

    chore: matakuliah

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -90,7 +90,7 @@
                         </span>
                     </li>
                     <li>
-                        <a href="<?= base_url('/dashboard/matakuliah') ?>" class="<?= current_url(true)->getSegment(1) == 'matakuliah' ? 'active' : '' ?>">
+                        <a href="<?= base_url('/dashboard/matakuliah') ?>" class="<?= current_url(true)->getSegment(2) == 'matakuliah' ? 'active' : '' ?>">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
